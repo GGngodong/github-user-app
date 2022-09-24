11d0fb52effe241ef23d1fb84ae60b884bba1e2f
GGngodong <ikkoy123@gmail.com>
2022-09-24 19:57:03 +0700

    cleanup FollowersFragment

--- app/src/main/java/com/ikkoy/githubuserapp/main/follow/FollowersFragment.kt
+++ app/src/main/java/com/ikkoy/githubuserapp/main/follow/FollowersFragment.kt
@@ -3,9 +3,12 @@ package com.ikkoy.githubuserapp.main.follow
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ikkoy.githubuserapp.R
 import com.ikkoy.githubuserapp.UserAdapter
 import com.ikkoy.githubuserapp.databinding.FragmentFollowBinding
+import com.ikkoy.githubuserapp.main.detail.DetailUsersActivity
 
 class FollowersFragment : Fragment(R.layout.fragment_follow) {
 
@@ -18,5 +21,35 @@ class FollowersFragment : Fragment(R.layout.fragment_follow) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentFollowBinding.bind(view)
+        setData()
+    }
+    private fun setData() {
+        val args = arguments
+        username = args?.getString(DetailUsersActivity.EXTRA_USERNAME).toString()
+
+        adapter = UserAdapter()
+        adapter.notifyDataSetChanged()
+
+        binding.apply {
+            fragmentRv.setHasFixedSize(true)
+            fragmentRv.layoutManager = LinearLayoutManager(activity)
+            fragmentRv.adapter = adapter
+        }
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        )[FollowersViewModel::class.java]
+        viewModel.getFollowers(username)
+        viewModel.getUserFollowers().observe(viewLifecycleOwner) {
+            if (it != null) {
+                adapter.setList(it)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
