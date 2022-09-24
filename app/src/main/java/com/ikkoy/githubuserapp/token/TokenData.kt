14af0573b839c55e283efec48dd49639f5afabb1
GGngodong <ikkoy123@gmail.com>
2022-09-24 19:01:46 +0700

    add data for token

---
+++ app/src/main/java/com/ikkoy/githubuserapp/token/TokenData.kt
@@ -0,0 +1,8 @@
+package com.ikkoy.githubuserapp.token
+
+class TokenData {
+    companion object {
+        const val GIT_TOKEN = "Rahasia bgt lo"
+        const val BASE_URL = "https://api.github.com/"
+    }
+}
